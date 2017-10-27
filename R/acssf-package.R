2272fe10dc18fcc08c66eb580b45e1eadbaf1ba0
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-10-27 14:26:24 -0400

    redo package documentation files

--- R/acssf-package.R
+++ R/acssf-package.R
@@ -3,6 +3,12 @@
 #' Download ACS Summary File data from FTP site, clean data and create tables in
 #' database.
 #'
+#' @docType package
+#' @name acssf
 #' @importFrom dplyr %>%
 #' @importFrom glue glue
-"_PACKAGE"
+NULL
+
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
