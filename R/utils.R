2fb678f692c946c0b6fa841f8c089484ec1ad23d
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-11-20 20:13:46 -0500

    fix envir issue with glue helpers

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,20 @@
 # Adapted from Jenny Bryan's googlesheets package
 
-cat_glue <- function(...) cat(glue::glue(..., .sep = "\n"))
-msg_glue <- function(...) message(glue::glue(...))
-warn_glue <- function(...) warning(glue::glue(...), call. = FALSE)
-stop_glue <- function(...) stop(glue::glue(...), call. = FALSE)
+msg_glue <- function(..., .envir = parent.frame()) {
+  message(glue::glue(..., .envir = .envir))
+}
+
+warn_glue <- function(...,.envir = parent.frame()) {
+  warning(glue::glue(..., .envir = .envir), call. = FALSE)
+}
+
+stop_glue <- function(..., .envir = parent.frame()) {
+  stop(glue::glue(..., .envir = .envir), call. = FALSE)
+}
+
 
 # Some functions get confused by type = "glue"
+# TODO: test on new glue release, bug should be fixed soon
 glue_chr <- function(..., .sep = "", .envir = parent.frame(), .open = "{", .close = "}") {
   as.character(glue::glue(..., .sep = .sep, .envir = .envir, .open = .open, .close = .close))
 }
