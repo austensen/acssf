390b8a1bd2ea31e2407913342e605688d00c9269
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-11-20 20:16:27 -0500

    add option to download 5yr data (tract/nontract geos)

--- R/download_helpers.R
+++ R/download_helpers.R
@@ -1,12 +1,15 @@
 
+# TODO: write bare-bones documentation for helper functions
 
 # scrape census ftp webpage for list of files to download
 scrape_filenames <- function(url, pattern = ".*") {
+
   xml2::read_html(url) %>%
     rvest::html_nodes("table td a") %>%
     rvest::html_attr("href") %>%
     .[-1] %>% # drop first element, always "parent dir"
-    purrr::keep(stringr::str_detect, pattern = pattern)
+    stringr::str_subset(pattern)
+
 }
 
 download_files <- function(urls, destfiles, ...) {
@@ -18,7 +21,9 @@ download_files <- function(urls, destfiles, ...) {
 
 # wrap unzip in walk to allow for unzipping multiple files, and preset arguments
 unzip_files <- function(zip_files, exdir, files = NULL, junkpaths = TRUE, ...) {
+
   purrr::walk(zip_files, utils::unzip, exdir = exdir, junkpaths = TRUE, ...)
+
 }
 
 # Downloads Seq/Table/Var Info
@@ -38,7 +43,10 @@ download_docs <- function(doc_dir, endyear, span) {
     if (endyear >= 2006) {
 
       docs_file_url <- dplyr::case_when(
-        endyear == 2006        ~ glue_chr("{docs_base_url}/merge_5_6_final.xls"),
+        # weird problem of wrong files in FTP for 2006, found correct version in
+        # different folder
+        # endyear == 2006        ~ glue_chr("{docs_base_url}/merge_5_6_final.xls"),
+        endyear == 2006        ~ "http://www2.census.gov/acs2006/merge_5_6_final.xls",
         endyear == 2007        ~ glue_chr("{docs_base_url}/{span}_year/merge_5_6_final.xls"),
         endyear %in% 2008:2009 ~ glue_chr("{docs_base_url}/{span}_year/user_tools/merge_5_6.xls"),
         endyear %in% 2010:2012 ~ glue_chr("{docs_base_url}/{span}_year/user_tools/Sequence_Number_and_Table_Number_Lookup.xls"),
@@ -74,22 +82,26 @@ download_docs <- function(doc_dir, endyear, span) {
     # for recent years get geography columns info
     if (endyear >= 2009) {
 
+      # TODO: 2013_5 was missing a geo template file, might be fine to just
+      # always download the 1yr version
+
       # File templates (for geography cols)
-      templates_filename <- ifelse(
-        endyear == 2010,
-        glue("{endyear}_{span}yr_SummaryFileTemplates.zip"),
-        glue("{endyear}_{span}yr_Summary_FileTemplates.zip")
+      templates_filename <- dplyr::case_when(
+        endyear == 2010 ~ glue_chr("{endyear}_1yr_SummaryFileTemplates.zip"),
+        TRUE            ~ glue_chr("{endyear}_1yr_Summary_FileTemplates.zip")
       )
 
       templates_url <- glue("{base_url}/data/{templates_filename}")
-      templates_file <- glue("{doc_dir}/{templates_filename}")
+
+      templates_file <- glue("{doc_dir}/{templates_filename}") %>%
+        stringr::str_replace("1yr", glue("{span}yr"))
 
       download_files(templates_url, templates_file)
 
       # unzip just the geography file
       zipped_geo_file <- utils::unzip(templates_file, list = TRUE) %>%
         dplyr::pull("Name") %>%
-        purrr::keep(stringr::str_detect, pattern = "SFGeoFileTemplate\\.xls$")
+        stringr::str_subset("SFGeoFileTemplate\\.xls$")
 
       utils::unzip(templates_file, files = zipped_geo_file, exdir = doc_dir, junkpaths = TRUE)
     }
@@ -100,24 +112,55 @@ download_docs <- function(doc_dir, endyear, span) {
 
 # Downloads Geography and Data Tables
 
-download_data <- function(geo_dir, endyear, span, geo_name) {
+download_data <- function(data_dir, endyear, span, geo_name) {
 
-  if (!file.exists(geo_dir)) {
+  # TODO: add warning if files already exist
+  if (!file.exists(data_dir)) {
 
-    dir.create(geo_dir, showWarnings = FALSE)
+    dir.create(data_dir, showWarnings = FALSE)
 
     base_url <- glue("https://www2.census.gov/programs-surveys/acs/summary_file/{endyear}")
 
     if (endyear >= 2009) {
 
-      # data files for all seqs
-      data_filename <- glue("{geo_name}_All_Geographies.zip")
+      if (span == 1L) {
 
-      data_url <- glue("{base_url}/data/{span}_year_by_state/{data_filename}")
-      data_file <- glue("{geo_dir}/{data_filename}")
+        data_filenames <- glue("{geo_name}_All_Geographies.zip")
 
-      download_files(data_url, data_file)
-      unzip_files(data_file, exdir = geo_dir)
+        data_files <- glue("{data_dir}/{data_filenames}")
+
+        data_urls <- glue("{base_url}/data/{span}_year_by_state/{data_filenames}")
+
+        download_files(data_urls, data_files)
+
+        unzip_files(data_files, exdir = data_dir)
+
+      } else if (span == 5L){
+
+
+        data_dir_non_tract <- glue("{data_dir}/non_tract_blockgroup")
+        data_dir_tract <- glue("{data_dir}/tract_blockgroup")
+
+        dir.create(data_dir_non_tract, showWarnings = FALSE)
+        dir.create(data_dir_tract, showWarnings = FALSE)
+
+        data_filenames <- c(
+          glue("{geo_name}_All_Geographies_Not_Tracts_Block_Groups.zip"),
+          glue("{geo_name}_Tracts_Block_Groups_Only.zip")
+        )
+
+        data_files <- c(
+          glue("{data_dir_non_tract}/{data_filenames[[1]]}"),
+          glue("{data_dir_tract}/{data_filenames[[2]]}")
+        )
+
+        data_urls <- glue("{base_url}/data/{span}_year_by_state/{data_filenames}")
+
+        download_files(data_urls, data_files)
+
+        unzip_files(data_files[[1]], exdir = data_dir_non_tract)
+        unzip_files(data_files[[2]], exdir = data_dir_tract)
+      }
 
 
     } else if (endyear <= 2008) {
@@ -138,11 +181,13 @@ download_data <- function(geo_dir, endyear, span, geo_name) {
       data_filenames <- scrape_filenames(data_base_url, data_file_pattern)
 
       data_urls <- glue('{data_base_url}/{data_filenames}')
-      data_files <- glue("{geo_dir}/{data_filenames}")
+      data_files <- glue("{data_dir}/{data_filenames}")
 
       download_files(data_urls, data_files)
-      # TODO:filter to only zipfiles
-      unzip_files(data_files, exdir = geo_dir)
+
+      data_files %>%
+        stringr::str_subset("\\.zip$") %>%
+        unzip_files(exdir = data_dir)
 
     }
   }
