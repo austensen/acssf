0d06b841d9e3ae5875e4aa15f62888ceeced4007
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-12-07 12:06:39 -0500

    add exported sum_level info frameata

---
+++ data-raw/summary_levels.R
@@ -0,0 +1,15 @@
+
+
+sum_level_info <- tibble::tribble(
+      ~geo_type, ~sum_level,  ~spans,    ~endyears,
+           "us",      "010",  "1, 5",  "2005-2016",
+         "cbsa",      "310",  "1, 5",  "2005-2016",
+        "place",      "160",  "1, 5",  "2005-2016",
+         "puma",      "795",  "1, 5",  "2005-2016",
+        "state",      "040",  "1, 5",  "2005-2016",
+       "county",      "050",  "1, 5",  "2005-2016",
+        "tract",      "140",     "5",  "2009-2016",
+   "blockgroup",      "150",     "5",  "2009-2016"
+)
+
+devtools::use_data(sum_level_info)
