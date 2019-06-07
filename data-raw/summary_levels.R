b8cfe83e9450c47881c76202e3b5b5046de2450c
Maxwell Austensen <maxwell.austensen@nyu.edu>
2019-06-07 12:23:58 -0400

    add zcta to summary levels

--- data-raw/summary_levels.R
+++ data-raw/summary_levels.R
@@ -4,6 +4,7 @@ sum_level_info <- tibble::tribble(
   ~geo_type, ~sum_level, ~spans, ~years,
           "us", "010", c(1, 5), 2005:2017,
         "cbsa", "310", c(1, 5), 2005:2017,
+        "zcta", "860", 5L,      2005:2017,
        "place", "160", c(1, 5), 2005:2017,
         "puma", "795", c(1, 5), 2005:2017,
        "state", "040", c(1, 5), 2005:2017,
